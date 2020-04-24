eb3a6b2f814af4635bb02a620e6326893563388b
deltaloose <takeshiroyu@gmail.com>
2020-04-24 18:23:57 +0900

    last mypage&edit

--- Bookers/app/controllers/application_controller.rb
+++ Bookers/app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ before_action :configure_permitted_parameters, if: :devise_controller?
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
+  def after_sign_in_path_for(resource)
+  	books_path
+  end
+  def after_sign_out_path_for(resource)
+  	root_path
+  end
 end
