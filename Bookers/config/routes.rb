acf7570d607456d92f2fb9e3aefb648048e4c78d
deltaloose <takeshiroyu@gmail.com>
2020-04-24 18:03:14 +0900

    add top

--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'books#top'
+  root to: 'top#top'
   resources :books, only: [:new, :create, :index, :show]
+  
+  devise_scope :user do
+  	get '/users/sign_out' => 'devise/sessons#destroy'
+  end
 end
